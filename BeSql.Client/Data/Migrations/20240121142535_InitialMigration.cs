85ccfee5a76cef8a3a35553e3892ec12ba7b0eb9
Andrew Wilkin <andrew.wilkin@blueostrichdesign.co.uk>
2024-01-21 16:03:03 +0000

    Migrations

---
+++ BeSql.Client/Data/Migrations/20240121142535_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeSql.Client.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CalculationResults",
+                columns: table => new
+                {
+                    OrganisationNodeId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MeasureId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MeasureMetricId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ReportMonth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalculationResults", x => x.OrganisationNodeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalculationResults");
+        }
+    }
+}
